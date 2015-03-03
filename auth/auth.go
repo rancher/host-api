574426d269a22c7063e907078fdff6f10678788e
Darren Shepherd <darren@rancher.com>
2015-03-03 10:43:15 -0700

    Refactor auth package for better reuse in host-proxy

--- auth/auth.go
+++ auth/auth.go
@@ -1,11 +1,14 @@
 package auth
 
 import (
+	"net/http"
+	"time"
+
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/golang/glog"
+	"github.com/gorilla/context"
 	"github.com/rancherio/host-api/app/common"
 	"github.com/rancherio/host-api/config"
-	"net/http"
 )
 
 func Auth(rw http.ResponseWriter, req *http.Request) bool {
@@ -32,10 +35,37 @@ func Auth(rw http.ResponseWriter, req *http.Request) bool {
 		return false
 	}
 
-	if token.Claims["hostUuid"] != config.Config.HostUuid {
+	if config.Config.HostUuidCheck && token.Claims["hostUuid"] != config.Config.HostUuid {
 		glog.Infoln("Host UUID mismatch , authentication failed")
 		return false
 	}
 
 	return true
+}
+
+func AuthHttpInterceptor(router http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		startTime := time.Now()
+
+		if !Auth(w, req) {
+			http.Error(w, "Failed authentication", 401)
+			return
+		}
+
+		router.ServeHTTP(w, req)
+
+		finishTime := time.Now()
+		elapsedTime := finishTime.Sub(startTime)
+
+		switch req.Method {
+		case "GET":
+			// We may not always want to StatusOK, but for the sake of
+			// this example we will
+			common.LogAccess(w, req, elapsedTime)
+		case "POST":
+			// here we might use http.StatusCreated
+		}
+
+		context.Clear(req)
+	})
 }
--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"time"
 
 	"github.com/rancherio/host-api/app/common"
 	"github.com/rancherio/host-api/auth"
@@ -29,7 +28,7 @@ func main() {
 	defer glog.Flush()
 
 	router := mux.NewRouter()
-	http.Handle("/", httpInterceptor(router))
+	http.Handle("/", auth.AuthHttpInterceptor(router))
 
 	router.Handle("/v1/stats", common.ErrorHandler(stats.GetStats)).Methods("GET")
 	router.Handle("/v1/stats/{id}", common.ErrorHandler(stats.GetStats)).Methods("GET")
@@ -42,30 +41,4 @@ func main() {
 		glog.Error(err)
 		os.Exit(1)
 	}
-}
-
-func httpInterceptor(router http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
-		startTime := time.Now()
-
-		if !auth.Auth(w, req) {
-			http.Error(w, "Failed authentication", 401)
-			return
-		}
-
-		router.ServeHTTP(w, req)
-
-		finishTime := time.Now()
-		elapsedTime := finishTime.Sub(startTime)
-
-		switch req.Method {
-		case "GET":
-			// We may not always want to StatusOK, but for the sake of
-			// this example we will
-			common.LogAccess(w, req, elapsedTime)
-		case "POST":
-			// here we might use http.StatusCreated
-		}
-
-	})
 }
