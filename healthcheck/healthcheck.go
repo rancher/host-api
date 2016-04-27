c1c52e49b6a628516dde3a0301f37c5ef48d004e
Alena Prokharchyk <alena@rancher.com>
2016-04-27 09:39:23 -0700

    Use LRU cache for storing haproxy stats

--- healthcheck/healthcheck.go
+++ healthcheck/healthcheck.go
@@ -6,6 +6,8 @@ import (
 	"time"
 
 	"github.com/Sirupsen/logrus"
+	set "github.com/deckarep/golang-set"
+	lru "github.com/hashicorp/golang-lru"
 	"github.com/rancherio/go-rancher/client"
 	"github.com/rancherio/host-api/pkg/haproxy"
 	"github.com/rancherio/host-api/util"
@@ -14,7 +16,6 @@ import (
 var PREFIX = "cattle-"
 var SERVER_NAME = "svname"
 var STATUS = "status"
-var LOOPS = (60 * 60) / 2
 
 func Poll() error {
 	client, err := util.GetRancherClient()
@@ -30,8 +31,23 @@ func Poll() error {
 		reportedStatus: map[string]string{},
 	}
 
-	for stat := range m.getStats() {
-		m.processStat(stat)
+	c, _ := lru.New(500)
+	go m.readStats(c)
+
+	for {
+		// Sleep here as well, so you don't end up reading from cache non stop
+		time.Sleep(2 * time.Second)
+		for _, key := range c.Keys() {
+			var stat haproxy.Stat
+			s, _ := c.Get(key)
+			if s == nil {
+				continue
+			}
+			if v, ok := s.(haproxy.Stat); ok {
+				stat = v
+				m.processStat(stat)
+			}
+		}
 	}
 
 	return nil
@@ -42,25 +58,16 @@ type Monitor struct {
 	reportedStatus map[string]string
 }
 
-func (m *Monitor) getStats() <-chan haproxy.Stat {
-	c := make(chan haproxy.Stat)
-	go m.readStats(c)
-	return c
-}
-
-func (m *Monitor) readStats(c chan<- haproxy.Stat) {
-	defer close(c)
-
+func (m *Monitor) readStats(c *lru.Cache) {
 	count := 0
 
 	h := &haproxy.Monitor{
 		SocketPath: haproxy.HAPROXY_SOCK,
 	}
 
-	for i := 0; i < LOOPS; i++ {
+	for {
 		// Sleep up front.  This way if this program gets restarted really fast we don't spam cattle
 		time.Sleep(2 * time.Second)
-
 		stats, err := h.Stats()
 		currentCount := 0
 		if err != nil {
@@ -68,10 +75,22 @@ func (m *Monitor) readStats(c chan<- haproxy.Stat) {
 			continue
 		}
 
+		servers := set.NewSet()
 		for _, stat := range stats {
 			if strings.HasPrefix(stat[SERVER_NAME], PREFIX) {
 				currentCount++
-				c <- stat
+				c.Add(stat[SERVER_NAME], stat)
+				servers.Add(stat[SERVER_NAME])
+			}
+		}
+
+		//cleanup obsolete cash entries
+		for _, key := range c.Keys() {
+			if s, ok := key.(string); ok {
+				if !servers.Contains(s) {
+					c.Remove(key)
+					logrus.Debugf("Removing obsolete entry from cache %v", s)
+				}
 			}
 		}
 
@@ -79,7 +98,6 @@ func (m *Monitor) readStats(c chan<- haproxy.Stat) {
 			count = currentCount
 			logrus.Infof("Monitoring %d backends", count)
 		}
-
 	}
 }
 
